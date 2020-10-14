b0a210f277e57844ec33ea2b82a88ecec5262dcb
Michael Johansen <michael.johansen@nav.no>
2020-10-14 19:34:29 +0200

    litt forenklet pipeline (#14)

--- src/cli/download-dist.js
+++ src/cli/download-dist.js
@@ -1,1 +1,14 @@
-const vers = require("../versioning/download-package");
+const getPackageJson = require('../utils/get-package-json');
+const rootDir = require('../utils/root-dir');
+const downloadPubPack = require('../versioning/download-published-package');
+
+const downloadDist = async () => {
+  const localPackageJson = getPackageJson(rootDir());
+  await downloadPubPack(localPackageJson.name);
+};
+
+
+downloadDist().then(()=>{
+  console.log("Done downloading latest package.");
+  process.exit(0);
+})
